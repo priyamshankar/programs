5601d7a3861e40c577c8378e71f796b1b76d5ec7
priyam <priyamshankar.5@gmail.com>
2022-02-23 20:11:17 +0530

    sorted check

---
+++ winter intersem1/DSA/checkSortedRecursion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+bool sortCheck(int *arr, int size)
+{
+    if (size == 1 || size == 0)
+    {
+        return true;
+    }
+    if (arr[0] > arr[1])
+    {
+        return false;
+    }
+    else
+    {
+        return sortCheck(arr + 1, --size);
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2,3, 5, 6, 7, 8};
+    // cout<<sortCheck(arr, 7);
+    
+    if (sortCheck(arr,7)){
+        cout<<"sorted";
+    }
+    else{
+        cout<<"notsorted";
+    }
+    return 0;
+}
