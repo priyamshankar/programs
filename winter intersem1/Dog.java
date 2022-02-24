bec487862e19de81eaf943c7185435cd36d3fdfa
priyam <priyamshankar.5@gmail.com>
2022-02-24 14:18:14 +0530

    cat2

---
+++ winter intersem1/Dog.java
@@ -0,0 +1,10 @@
+public class Dog
+{
+    public static void main(String[] args)
+    {
+       String s1 = "Java";
+       String s2 = "Java";
+       System.out.println(s1.equals(s2));
+       System.out.println(s1==s2);
+    }
+}
