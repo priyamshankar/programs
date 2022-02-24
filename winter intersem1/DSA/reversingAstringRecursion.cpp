4c5d689879e4cf6d11ef76532e2d7b4c9f423799
priyam <priyamshankar.5@gmail.com>
2022-02-25 03:14:48 +0530

    reverse a string

--- winter intersem1/DSA/reversingAstringRecursion.cpp
+++ winter intersem1/DSA/reversingAstringRecursion.cpp
@@ -1,10 +1,28 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
+// int reverse(string str, int size, int count)
+// {
+//     if (count == size)
+//     {
+//         return str;
+//     }
+//     // s = str[count];
+//     reverse(str, size, count+1);
+//     return str ;
+// }
 
+// int main()
+// {
+//     string str = "priyam";
+//     int size = 6;
+//     int count = 0;
+//     // char s;
+//     cout << reverse(str, size, count);
 
-int main()
-{
-
-    return 0;
-}
+//     return 0;
+// }
+// //approach sahi hai code galat hai
+// //approach sahi hai code galat hai
+// //approach sahi hai code galat hai
+// //approach sahi hai code galat hai
