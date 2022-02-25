d48af194eebdaaa6ec7987800f128e2b32e5f010
priyam <priyamshankar.5@gmail.com>
2022-02-25 23:25:09 +0530

    no.of path in a maze

---
+++ winter intersem1/DSA/permutationStringRecursion2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void permutation(string str, string ans)
+{
+    if (str.length() == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        char ch = str[i];
+        // cout << ch << endl;
+        string ros = str.substr(0, i) + str.substr(i + 1);
+
+        permutation(ros, ans + str[i]);
+    }
+}
+
+int main()
+{
+    permutation("abc", "");
+    return 0;
+}
