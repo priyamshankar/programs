23e1235fc9daabf3d5332b040fad057e16ce9fbd
priyam <priyamshankar.5@gmail.com>
2022-03-06 02:21:46 +0530

    reversing the matrix

---
+++ winter intersem1/class12Questions/MatRev.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+class MatRev {
+    private int arr[][];
+    private int m;
+    private int n;
+
+    public MatRev(int mm, int nn) {
+        m = mm;
+        n = nn;
+        arr = new int[m][n];
+    }
+
+    public void fillArray() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter matrix elements::");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    public int reverse(int x) {
+        int rev = 0;
+        for (int i = x; i != 0; i /= 10)
+            rev = rev * 10 + i % 10;
+        return rev;
+    }
+
+    public void revMat(MatRev p) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                this.arr[i][j] = reverse(p.arr[i][j]);
+            }
+        }
+    }
+
+    public void show() {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+public static void main(String args[])throws IOException{
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter number of rows::");
+int x = sc.nextInt();
+System.out.print("Enter number of columns::");
+int y = sc.nextInt();
+MatRev obj1 = new MatRev(x, y);
+MatRev obj2 = new MatRev(x, y);
+obj1.fillArray();
+obj2.revMat(obj1);
+System.out.println("Original Matrix is::");
+obj1.show();
+System.out.println("Matrix with reversed elements");
+obj2.show();
+}
+}
---
+++ winter intersem1/class12Questions/armstrong.java
@@ -0,0 +1,45 @@
+
+// import java.util.*;
+import java.lang.Math;
+
+class ArmNum {
+    protected int n;
+    protected int i;
+    protected int sum;
+    int temp;
+
+    public void armstrong() {
+        if (sum_pow() == n) {
+            System.out.println("It is an armstrong no.");
+        } else {
+            System.out.println("Not an armstrong");
+        }
+        System.out.println(sum);
+    }
+
+    int sum_pow() {
+        sum = 0;
+        temp = n;
+        for (int j = 0; j < i; j++) {
+            int last;
+            last = temp % 10;
+            sum += (Math.pow(last, i));
+            temp = temp / 10;
+        }
+        return sum;
+    }
+
+    ArmNum(int no, int dig) {
+        this.n = no;
+        this.i = dig;
+    }
+}
+
+public class armstrong {
+    public static void main(String[] args) {
+        ArmNum initial = new ArmNum(371, 3);
+        ArmNum initial2=new ArmNum(254, 3);
+        initial2.armstrong();
+        initial.armstrong();
+    }
+}
