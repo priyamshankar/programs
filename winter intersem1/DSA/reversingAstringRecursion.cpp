7d9a5b1cebfbf279cb0074bb92b8e5274bfd9ea7
priyam <priyamshankar.5@gmail.com>
2022-03-13 15:23:25 +0530

    string reverse using recursion fixed

--- winter intersem1/DSA/reversingAstringRecursion.cpp
+++ winter intersem1/DSA/reversingAstringRecursion.cpp
@@ -1,28 +1,26 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int reverse(string str, int size, int count)
-// {
-//     if (count == size)
-//     {
-//         return str;
-//     }
-//     // s = str[count];
-//     reverse(str, size, count+1);
-//     return str ;
-// }
+string reverse(string str, int size, int count)
+{
+    if (count == size)
+    {
+        return str;
+    }
+    char s = str[count];
+    reverse(str, size, count + 1);
+    cout << s;
+    return str;
+}
 
-// int main()
-// {
-//     string str = "priyam";
-//     int size = 6;
-//     int count = 0;
-//     // char s;
-//     cout << reverse(str, size, count);
+int main()
+{
+    string str = "priyam";
+    int size = 6;
+    int count = 0;
+    // char s;
+    // cout << reverse(str, size, count);
+    reverse(str, size, count);
 
-//     return 0;
-// }
-// //approach sahi hai code galat hai
-// //approach sahi hai code galat hai
-// //approach sahi hai code galat hai
-// //approach sahi hai code galat hai
+    return 0;
+}
