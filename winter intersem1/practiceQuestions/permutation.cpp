abd97736cda879c9877280a5fb19d4af19867a85
priyam <priyamshankar.5@gmail.com>
2022-05-24 16:54:00 +0530

    phone book problem try

---
+++ winter intersem1/practiceQuestions/permutation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void permute(string s, int i)
+{
+    if (i == s.length())
+    {
+        cout << s << " ";
+        return;
+    }
+
+    for (int j = i; j < s.length(); j++)
+    {
+        swap(s[i], s[j]);
+        permute(s, i + 1);
+        swap(s[i], s[j]);
+    }
+}
+
+int main()
+{
+
+    string s = "abc";
+    permute(s, 0);
+    return 0;
+}
