9ec7e27ab2823f3b2f6a5f53175fde85ee529c2a
priyam <priyamshankar.5@gmail.com>
2022-05-27 01:13:54 +0530

    testfiels

---
+++ summerSem/test.java
@@ -0,0 +1,14 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int g=1;
+        // System.out.println(g++*2);
+        // System.out.println(30+21+"sadf");
+        // char ch = ;
+        System.out.println(-8>>1);
+        
+    }
+}
