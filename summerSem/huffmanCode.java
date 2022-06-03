a1711b3c4fb8cacec312b9dfa6dc3270c86d797e
priyam <priyamshankar.5@gmail.com>
2022-06-03 17:13:12 +0530

    class work

---
+++ summerSem/huffmanCode.java
@@ -0,0 +1,15 @@
+public class huffmanCode {
+    /*
+     * greedy algo
+     * encoding/decoding
+     * fixed variable
+     * unfixed variable
+     * 
+     * ababaccdef
+     * freq a=3
+     * freq b=2
+     * freq c=2
+     * freq d=1
+     * freq e=1
+     */
+}
