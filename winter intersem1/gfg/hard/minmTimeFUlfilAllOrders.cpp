a0589a0f807aed12e37a3b42fbc94c2fe1b4f6ae
priyam <priyamshankar.5@gmail.com>
2022-09-03 23:31:56 +0530

    gfg minm time to fulifl order

---
+++ winter intersem1/gfg/hard/minmTimeFUlfilAllOrders.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    int findMinTime(int N, vector<int> &A, int L)
+    {
+        // write your code here
+        int times = 1;
+        int count = 0;
+        while (count != N)
+        {
+            for (int i = 0; i < L; i++)
+            {
+                if (times % A[i] == 0)
+                {
+                    count++;
+                    // cout<<i<<"-"<<count;
+                }
+            }
+
+            times++;
+        }
+        return times;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int n;
+    cin >> n;
+    int l;
+    cin >> l;
+    vector<int> arr(l);
+    for (int i = 0; i < l; i++)
+    {
+        cin >> arr[i];
+    }
+    Solution ob;
+    int ans = ob.findMinTime(n, arr, l);
+    cout << ans << endl;
+
+    return 0;
+}
+
+// } Driver Code Ends
